9f2b57e3646f44192eb6b549d162f0ebec9b05a0
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-06-20 16:51:41 +0300

    Added new file for sharing context over multiple files

---
+++ src/contexts/TodoAppContexts.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const TodoToggleCbContext = React.createContext();
+const TodoDeleteCbContext = React.createContext();
+
+export { TodoDeleteCbContext, TodoToggleCbContext };
