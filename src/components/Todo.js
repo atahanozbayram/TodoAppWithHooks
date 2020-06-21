6d224dd61e6c87142d049b048d92bee859f2a148
Atahan Ozbayram <atahan_ozbayram@hotmail.com>
2020-06-21 06:19:27 +0300

    I made changes to two components
    I made changes to two components which are Todo and TodoApp. Details are
    as follows:
    
    1. Todo: Added few lines of code to achieve toggling of the completed
       property of the todo and deleting the todo using React context.
    
    2. TodoApp: Added two new contexts and provided them for TodoList
       element and under.

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { TodoDeleteCbContext, TodoToggleCbContext } from '../contexts/TodoAppContexts';
 
 function Todo(props) {
-  const { text } = props;
-  return <div>{text}</div>;
+	const { id, text, completed } = props;
+	return (
+		<div>
+			<p>{text}</p>
+			<TodoToggleCbContext.Consumer>
+				{(callback) => {
+					return <button onClick={callback.bind(null, id)}>{completed ? 'UNCOMPLETE' : 'COMPLETE'}</button>;
+				}}
+			</TodoToggleCbContext.Consumer>
+			<TodoDeleteCbContext.Consumer>
+				{(callback) => {
+					return <button onClick={callback.bind(null, id)}>DELETE</button>;
+				}}
+			</TodoDeleteCbContext.Consumer>
+		</div>
+	);
 }
 
 // Prop types is good for debugging props provided to Todo
 Todo.propTypes = {
-  id: PropTypes.number.isRequired,
-  text: PropTypes.string.isRequired,
-  completed: PropTypes.bool.isRequired,
+	id: PropTypes.number.isRequired,
+	text: PropTypes.string.isRequired,
+	completed: PropTypes.bool.isRequired,
 };
 
 export default Todo;
--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -2,6 +2,7 @@ import React from 'react';
 import TodoHeader from '../components/TodoHeader';
 import TodoAdd from '../components/TodoAdd';
 import TodoList from '../components/TodoList';
+import { TodoDeleteCbContext, TodoToggleCbContext } from '../contexts/TodoAppContexts';
 
 function TodoApp(props) {
 	const [state, setState] = React.useState({
@@ -22,37 +23,41 @@ function TodoApp(props) {
 
 			return Object.assign({}, currentState, { todoCounter: currentState.todoCounter + 1 });
 		});
+	}
 
-		// Function to be calld for deleting todo from the todoArray
-		function todoDeleteCbFunction(todoId) {
-			setState((currentState) => {
-				return Object.assign({}, currentState, {
-					todoArray: currentState.todoArray.filter((todo) => {
-						if (todo.id === todoId) return false; // if the id matches return false so it will remove the todo from array
-						return true; // if the id doesn't match return true, so it will keep the todo in the array
-					}),
-				});
+	// Function to be calld for deleting todo from the todoArray
+	function todoDeleteCbFunction(todoId) {
+		setState((currentState) => {
+			return Object.assign({}, currentState, {
+				todoArray: currentState.todoArray.filter((todo) => {
+					if (todo.id === todoId) return false; // if the id matches return false so it will remove the todo from array
+					return true; // if the id doesn't match return true, so it will keep the todo in the array
+				}),
 			});
-		}
+		});
+	}
 
-		// Function to be called for toggling completed property of given todoId's matching todo
-		function todoToggleCbFunction(todoId) {
-			setState((currentState) => {
-				Object.assign({}, currentState, {
-					todoArray: currentState.todoArray.map((todo) => {
-						if (todo.id === todoId) Object.assign({}, todo, { completed: !todo.completed });
-						return todo;
-					}),
-				});
+	// Function to be called for toggling completed property of given todoId's matching todo
+	function todoToggleCbFunction(todoId) {
+		setState((currentState) => {
+			return Object.assign({}, currentState, {
+				todoArray: currentState.todoArray.map((todo) => {
+					if (todo.id === todoId) return Object.assign(todo, { completed: !todo.completed });
+					return todo;
+				}),
 			});
-		}
+		});
 	}
 
 	return (
 		<div>
 			<TodoHeader />
 			<TodoAdd todoAddCb={todoAddCbFunction} />
-			<TodoList todoArray={state.todoArray} />
+			<TodoDeleteCbContext.Provider value={todoDeleteCbFunction}>
+				<TodoToggleCbContext.Provider value={todoToggleCbFunction}>
+					<TodoList todoArray={state.todoArray} />
+				</TodoToggleCbContext.Provider>
+			</TodoDeleteCbContext.Provider>
 		</div>
 	);
 }
